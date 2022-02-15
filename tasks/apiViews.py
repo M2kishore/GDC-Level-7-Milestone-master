2e994db9b5af4e318776a5c1726869af3335058d
M M Kishore <cb.en.u4cse19127@cb.students.amrita.edu>
2022-02-15 11:56:35 +0530

    initial commit completed filter added

---
+++ tasks/apiViews.py
@@ -0,0 +1,74 @@
+from ast import Mod
+from django.views import View
+
+from django.contrib.auth.models import User
+
+from django.http.response import JsonResponse
+
+from tasks.models import Task
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.serializers import ModelSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from django_filters.rest_framework import (
+    DjangoFilterBackend,
+    FilterSet,
+    CharFilter,
+    ChoiceFilter,
+    BooleanFilter,
+)
+
+
+STATUS_CHOICES = (
+    ("PENDING", "PENDING"),
+    ("IN_PROGRESS", "IN_PROGRESS"),
+    ("COMPLETED", "COMPLETED"),
+    ("CANCELLED", "CANCELLED"),
+)
+
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "username"]
+
+
+class TaskSerializer(ModelSerializer):
+
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Task
+        fields = ["title", "description", "completed", "user"]
+
+
+class TaskFilter(FilterSet):
+    title = CharFilter(lookup_expr="icontains")
+    status = ChoiceFilter(choices=STATUS_CHOICES)
+    completed = BooleanFilter()
+
+
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    permission_classes = (IsAuthenticated,)
+
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = TaskFilter
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user, deleted=False)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class TaskListAPI(APIView):
+    def get(self, response):
+        tasks = Task.objects.filter(deleted=False)
+        data = TaskSerializer(tasks, many=True).data
+        return Response({"tasks": data})
